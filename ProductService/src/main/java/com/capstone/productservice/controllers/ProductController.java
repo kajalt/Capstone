7d11fcc977d18dc01f2e5f601d564f5583f9a7b9
Kajal <kajal17.kt@gmail.com>
2025-03-30 22:15:31 +0530

    added custom headers and experimenting with ResponseEntity and statuses

--- ProductService/src/main/java/com/capstone/productservice/controllers/ProductController.java
+++ ProductService/src/main/java/com/capstone/productservice/controllers/ProductController.java
@@ -3,6 +3,11 @@ package com.capstone.productservice.controllers;
 import com.capstone.productservice.dto.ProductDTO;
 import com.capstone.productservice.models.Product;
 import com.capstone.productservice.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +15,7 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    @Autowired
     private ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -21,14 +27,24 @@ public class ProductController {
         return null;
     }
 
-    @GetMapping("products/{id}")
-    public ProductDTO getProductById(@PathVariable long id) {
-        Product product = productService.getProduct(id);
-        return new ProductDTO(product);
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long productId) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("called by", "smart frontend"); // comes for right as well as wrong input val
+        try {
+            if (productId < 1) {
+                headers.add("called by", "wrong frontend");
+                throw new IllegalArgumentException("id is invalid");
+            }
+            Product product = productService.getProduct(productId);
+            return new ResponseEntity<>(product, headers, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/products/create")
-    public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
-        return productDTO;
+    public ProductDTO createProduct(@RequestBody ProductDTO productDto) {
+       return productService.createProduct(productDto);
     }
 }
